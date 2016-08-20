3aa279d1b4cf725a6eef6d5d6dd480472734f763
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2016-08-20 09:46:08 -0300

    Just read CSV on specific format
    Instead of allowing column naming and flexible positions, fix a CSV
    format. This makes like easier and is not hard at all for the user to
    format its own CSV.

--- spec/data/reader/csv_to_data_spec.rb
+++ spec/data/reader/csv_to_data_spec.rb
@@ -1,14 +1,13 @@
 require 'spec_helper'
 
 describe XpTeamWise::Data::Reader::CSVToData do
-  describe 'initialization' do
-    let(:path) { '/test/test.csv' }
-    let(:first_row) { ['name', 'email', 'coach', 'project1', 'project2', 'project3'] }
+  describe 'class method' do
+    describe 'read' do
+      it 'is expected to read the sample data' do
+        result = described_class.read('sample.csv')
 
-    it 'is expected to load the CSV first column' do
-      expect(CSV).to receive(:read).with(path).and_return([first_row])
-
-      described_class.new(path)
+        expect(result).to all(be_a(XpTeamWise::Data::Member))
+      end
     end
   end
 end
--- src/data/reader/csv_to_data.rb
+++ src/data/reader/csv_to_data.rb
@@ -4,32 +4,24 @@ module XpTeamWise
   module Data
     module Reader
       class CSVToData # Longer name to avoid name clash with Ruby's CSV class
-        def initialize(path, name_col: 'name', email_col: 'email', coach_col: 'coach', first_project_col: 'project1', second_project_col: 'project2', third_project_col: 'project3')
-          @path = path
+        class << self
+          def read(path)
+            members = []
 
-          @name_col = nil
-          @email_col = nil
-          @coach_col = nil
-          @first_project_col = nil
-          @second_project_col = nil
-          @third_project_col = nil
+            CSV.foreach(path) do |row|
+              project1 = XpTeamWise::Data::Project.new(name: row[3])
+              project2 = XpTeamWise::Data::Project.new(name: row[4])
+              project3 = XpTeamWise::Data::Project.new(name: row[5])
 
-          CSV.read(@path).first.each.with_index do |col, index|
-            case col
-              when name_col
-                @name_col = index
-              when email_col
-                @email_col = index
-              when coach_col
-                @coach_col = index
-              when first_project_col
-                @first_project_col = index
-              when second_project_col
-                @second_project_col = index
-              when third_project_col
-                @third_project_col = index
+              members << XpTeamWise::Data::Member.new(name: row[0],
+                                                      email: row[1],
+                                                      coach: (row[2] == 'true'),
+                                                      projects: [project1, project2, project3])
             end
+
+            members
           end
+
         end
       end
     end
