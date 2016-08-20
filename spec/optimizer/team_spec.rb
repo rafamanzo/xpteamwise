ecfc9c2ffddf45f3f93a073456e7e141485f5efb
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2016-08-20 12:25:28 -0300

    Add string conversion to team

--- spec/optimizer/team_spec.rb
+++ spec/optimizer/team_spec.rb
@@ -2,10 +2,23 @@ require 'spec_helper'
 require 'support/shared_examples_for_attributes'
 
 describe XpTeamWise::Optimizer::Team do
-  subject { described_class.new(FactoryGirl.build(:project), [FactoryGirl.build(:member)]) }
+  let(:project) { FactoryGirl.build(:project) }
+  let(:member) { FactoryGirl.build(:member, projects: [project]) }
+  let(:coach) { FactoryGirl.build(:member, :coach, projects: [project]) }
+  subject { described_class.new(project, [member, coach]) }
 
   describe 'attribute' do
     include_examples 'has attribute', 'project'
     include_examples 'has attribute', 'members'
+  end
+
+  describe 'method' do
+    describe 'to_s' do
+      it 'is expected to print the team information into a string' do
+        expectation = "#{project.name}\n\t- #{member.name}\n\t* #{coach.name}"
+
+        expect(subject.to_s).to eq(expectation)
+      end
+    end
   end
 end
--- src/optimizer/team.rb
+++ src/optimizer/team.rb
@@ -7,6 +7,18 @@ module XpTeamWise
         @project = project
         @members = members
       end
+
+      def to_s
+        str = "#{project.name}"
+
+        members.each do |member|
+          str << "\n\t"
+          str << (member.coach ? "* " : "- ")
+          str << member.name
+        end
+
+        str
+      end
     end
   end
 end
